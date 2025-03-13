120637d8909af05d7ddb2704f51ded26af7c4c16
aniljoseph-ae <aniljoseph.ae@gmail.com>
2025-03-13 17:09:51 +0530

    update 01

---
+++ models/image_to_image.py
@@ -0,0 +1,27 @@
+import torch
+import os
+from dotenv import load_dotenv
+from PIL import Image
+from diffusers import StableDiffusionInpaintPipeline
+
+# Load API Key
+load_dotenv()
+access_token = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+# Check if GPU is available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def load_inpainting_model():
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2-inpainting",
+        torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+        use_auth_token=access_token
+    )
+    pipe.to(device)
+    return pipe
+
+inpainting_pipeline = load_inpainting_model()
+
+def generate_image_from_image(image, mask, prompt):
+    image = inpainting_pipeline(prompt=prompt, image=image, mask_image=mask).images[0]
+    return image
---
+++ models/text_to_image.py
@@ -0,0 +1,27 @@
+import torch
+import os
+from dotenv import load_dotenv
+from diffusers import StableDiffusionPipeline
+
+# Load API Key
+load_dotenv()
+access_token = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+# Check if GPU is available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def load_text_to_image_model():
+    pipe = StableDiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2",
+        torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+        use_auth_token=access_token
+    )
+    pipe.to(device)
+    pipe.enable_attention_slicing()
+    return pipe
+
+text_to_image_pipeline = load_text_to_image_model()
+
+def generate_image_from_text(prompt):
+    image = text_to_image_pipeline(prompt).images[0]
+    return image
